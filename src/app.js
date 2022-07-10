04952490213c8d999595edee2bef87184364c9ae
cloudoor <67406332+cloudoor@users.noreply.github.com>
2022-07-10 14:07:10 +0000

    v0.3.14

--- src/app.js
+++ src/app.js
@@ -1,2 +1,19 @@
 
 import { LitElement, html } from "./lit.js";
+
+class EpistemologyElement extends LitElement {
+  messages = [];
+
+  pending = false;
+
+  constructor() {
+    super();
+  }
+
+  async sendMessage() {
+    const input = this.querySelector("#user-input");
+    const message = input.value;
+    const context = this.querySelector("#context").value;
+    let newMessages = [...this.messages];
+    newMessages.push({
+      role: "user",
