182aa3ae297d48885d1038ddf8508b51f654f42a
cloudoor <67406332+cloudoor@users.noreply.github.com>
2022-07-23 10:46:39 +0000

    chore(package): update babel-core to version 6.14.0

--- src/lit.js
+++ src/lit.js
@@ -8,3 +8,13 @@ const t = globalThis,
   s =
     t.ShadowRoot &&
     (void 0 === t.ShadyCSS || t.ShadyCSS.nativeShadow) &&
+    "adoptedStyleSheets" in Document.prototype &&
+    "replace" in CSSStyleSheet.prototype,
+  i = Symbol(),
+  e = new WeakMap();
+class o {
+  constructor(t, s, e) {
+    if (((this._$cssResult$ = !0), e !== i))
+      throw Error(
+        "CSSResult is not constructable. Use `unsafeCSS` or `css` instead."
+      );
