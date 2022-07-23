2acb5f7fc1f29f718d1cfe7a646da0fbe1fe4647
cloudoor <67406332+cloudoor@users.noreply.github.com>
2022-07-23 09:50:31 +0000

    Updated Guide

---
+++ src/lit.js
@@ -0,0 +1,10 @@
+
+/**
+ * @license
+ * Copyright 2019 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+const t = globalThis,
+  s =
+    t.ShadowRoot &&
+    (void 0 === t.ShadyCSS || t.ShadyCSS.nativeShadow) &&
